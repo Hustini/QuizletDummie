6f7a0041cf6d233b9d4f307cee1945a77b451d31
Hustini <ejdersalih02@gmail.com>
2024-11-12 15:11:43 +0100

    added comment to make code more readable and removed unused code pieces

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Main {
-
-    static JButton button;
-    static JTextField textField;
-
     public static void main(String[] args) {
 
         JPanel panel = new JPanel();
@@ -15,6 +11,7 @@ public class Main {
         int screenWidth = 600;
         int screenHeight = 400;
 
+        // Standard frame
         JFrame frame = new JFrame("Quizlet Dummie");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(screenWidth, screenHeight);
@@ -23,16 +20,19 @@ public class Main {
 
         panel.setLayout(null);
 
+        // Label for a title
         JLabel label = new JLabel("Quizlet Dummie");
         label.setFont(new Font("Times New Roman", Font.PLAIN, 16));
         label.setBounds(230, 0, screenWidth, screenHeight / 10);
         panel.add(label);
 
-        textField = new JTextField();
+        //Text field to get user input
+        JTextField textField = new JTextField();
         textField.setBounds(100, 100, 300, 25);
         panel.add(textField);
 
-        button = new JButton("Check");
+        // Button to check user input
+        JButton button = new JButton("Check");
         button.setBounds(420, 100, 80, 25);
         panel.add(button);
 
